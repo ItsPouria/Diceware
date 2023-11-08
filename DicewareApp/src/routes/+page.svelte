82a861cbcd342cddf79822b78120d77ebf9639c0
ItsPouria <92713967+ItsPouria@users.noreply.github.com>
2023-11-08 13:35:30 +0000

    Clean up the main page code

--- DicewareApp/src/routes/+page.svelte
+++ DicewareApp/src/routes/+page.svelte
@@ -2,8 +2,6 @@
     import Dice from "../components/Dice.svelte";
 
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <Dice>
-    
+
 </Dice>
